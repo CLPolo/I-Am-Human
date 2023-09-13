1ad247d458d5467bc3c05572a92a2c34bfd844d3
GurtajK <gurtaj@ualberta.ca>
2023-09-12 20:48:58 -0600

    added base unity project

---
+++ Unity/Misery Loves Co. Prototype/Assets/Scripts/LevelDesignPlatformerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFeel{
+    public class LevelDesignPlatformerScript : AudioPlatformerScriptV2
+    {
+        [Header("Level Settings")]
+        public AudioSource audioSourceMusic; // audio source for background music
+        public AudioClip backgroundMusic; // clip for background music
+
+        protected override void Start(){
+            base.Start();
+            // if we have background music, start it!
+            if (audioSourceMusic != null && backgroundMusic != null){
+                audioSourceMusic.clip = backgroundMusic;
+                audioSourceMusic.Play();
+            }
+        }
+
+        protected override void OnGrounded_Hook(){
+            base.OnGrounded_Hook();
+            // check if we've landed on a platform, and send a signal if so!
+            Vector2 extents = _playerCollider.bounds.extents;
+            Vector2 rayPosition = new Vector2(transform.position.x, transform.position.y - extents.y); // reminder to unhack once yalmaz makes collider protected
+            RaycastHit2D checkValid = Physics2D.Raycast(rayPosition, Vector2.down);
+
+            Debug.Log(checkValid.collider);
+            if (checkValid.collider){
+                checkValid.collider.gameObject.SendMessage("onContact");
+            }
+
+        }
+    }
+
+
+    
+}
