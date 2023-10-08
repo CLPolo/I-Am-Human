f6d122c8851c6f57bd0b4ec04419d5f14b0eeffc
unknown <mbmartin@ualberta.ca>
2023-10-08 15:02:55 -0600

    added player running and basic scene switch, as well as re-added hiding and combined it with push/pull

---
+++ Unity/TechDemo/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Level Exit")
+        {
+            if (SceneManager.GetActiveScene().name == "Running Area")
+            {
+                SceneManager.LoadScene("Grab Test");
+            }
+            else
+            {   
+                SceneManager.LoadScene("Running Area");
+            }    
+        }
+    }
+
+
+}
