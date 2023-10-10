c88b829d2d65d5a41df52d12820e744a53fc1fb1
unknown <mbmartin@ualberta.ca>
2023-10-09 19:04:52 -0600

    added flashback display within interactable object script & cleaned up the interactable object script

--- Unity/TechDemo/Assets/Scripts/InteractableObjectScript.cs
+++ Unity/TechDemo/Assets/Scripts/InteractableObjectScript.cs
@@ -4,62 +4,81 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System;
+using UnityEditor.U2D;
 
 public class InteractableObjectScript : MonoBehaviour
 {
 
+    [Header("Interaction")]
     public GameObject InteractableScreen;  // this is the screen that displays prompt to interact
-    public GameObject InfoScreen;  // this is the screen that displays info about object post interaction
-    public List<String> InfoMessages;
     public String InteractMessage;
-    public int InfoTime = 3;  // time in seconds that info will be displayed for
     public KeyCode InteractKey;  // Keycode for the chosen interact key
     public Boolean ShowPromptOnce;
-    public bool monsterComing = false;  // This is for activating Monster.cs
-    public AudioSource rawr;
 
+    [Header("Info Display")]
+    public GameObject InfoScreen;  // this is the screen that displays info about object post interaction
+    public List<String> InfoMessages;
+    public int InfoTime = 3;  // time in seconds that info will be displayed for
+
+    [Header("Flashback Sequences")]
+    public List<Sprite> FlashbackImages;  // all images for the flashback sequence
+    public GameObject FlashbackScreen;  // screen that displays the flashback images
+    public int FlashbackTime = 4;  // time between images in sequence (in seconds)
+
+    // Booleans
     private bool Inside = false;  // if player is inside hit box for object
     private bool HasInteracted = false;  // if player interacted with object
+    private bool IsFlashback = false;  // if the post interaction display is a flashback sequence
+
+
+    // Below Should be removed and placed into monster script
+    public bool monsterComing = false;  // This is for activating Monster.cs
+    //public AudioSource rawr;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        // turns off all screens
         InteractableScreen.SetActive(false);
         if (InfoScreen != null)
         {
             InfoScreen.SetActive(false);
+        }
+        if (FlashbackScreen != null)
+        {
+            FlashbackScreen.SetActive(false);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Inside) // if player is inside the interactable object's box collider
+        if (Inside && Input.GetKey(InteractKey)) // if player is inside the interactable object's box collider
         {
-            // if they press InteractKey to interact & haven't already interacted with object
-            // and we only want the interact prompt to appear once
-            if (Input.GetKey(InteractKey))
+            InteractableScreen.SetActive(false);  // turns off 'interact' prompt
+            CheckAndDisplayInfo();  // checks if there's info to display, if so does that
+            if (IsFlashback)  // If post interaction display is a flashback sequence
             {
-                InteractableScreen.SetActive(false);  // turns off 'interact' prompt
-                if (InfoScreen != null && InfoMessages.Count > 0)
-                {
-                    StartCoroutine(InfoInteraction()); // post interaction function
-                }
-                HasInteracted = true;  // Show interact prompt only once
+                CheckAndDisplayFlashback();  // checks and displays flashback sequence
             }
         }
     }
 
+    // if they press InteractKey to interact & haven't already interacted with object and we only want the interact prompt to appear once
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         // if we want prompt to always show or the player has never interacted, then show prompt
-        Inside = true && !Input.GetKeyDown(InteractKey);
-        if (!HasInteracted || !ShowPromptOnce)
+        if (tag == "Flashback")  // NOTE: object that triggers flashback sequence shouldbe tagged as 'Flashback'
         {
-            var interactText = InteractableScreen.GetComponentInChildren<Text>();
-            interactText.text = InteractMessage;
-            Interactable();
+            IsFlashback = true;
         }
+        if (Inside = true && !Input.GetKeyDown(InteractKey))  // if player in collider and has NOT pressed interact key yet
+        {
+            DisplayInteractPrompt();  // shows the interact prompt
+        }
+        
     }
 
     private void OnTriggerExit2D(Collider2D col)
@@ -69,14 +88,44 @@ public class InteractableObjectScript : MonoBehaviour
         Interactable();
     }
 
+    private void DisplayInteractPrompt()
+    {
+        if (!HasInteracted || !ShowPromptOnce)  // if they haven't already interacted and they aren't limited to interacting once only
+        {
+            // displays interact text
+            var interactText = InteractableScreen.GetComponentInChildren<Text>();
+            interactText.text = InteractMessage;
+            Interactable();  // turns on the interact screen
+        }
+    }
+
+    private void CheckAndDisplayInfo()
+    {
+        // if there's info to display it will display it
+        if (InfoScreen != null && InfoMessages.Count > 0)
+        {
+            StartCoroutine(DisplayInfo()); // post interaction function
+        }
+        HasInteracted = true;  // Show interact prompt only once
+    }
+
+    private void CheckAndDisplayFlashback()
+    {
+        // if there's a flashback to display it will display it
+        if (FlashbackScreen != null && FlashbackImages.Count > 0)
+        {
+            StartCoroutine(DisplayFlashback()); // post interaction function
+        }
+        HasInteracted = true;  // Show interact prompt only once
+    }
+
     private void Interactable()
     {
-        // turns interact prompt on or off depending on state provided
-        // bool state - state to determine if interact prompt is on or off
+        // turns interact prompt on or off depending on whether player is inside
         InteractableScreen.SetActive(Inside);
     }
 
-    private IEnumerator InfoInteraction()
+    private IEnumerator DisplayInfo()
     {
         // Set InfoScreen to active
         InfoScreen.SetActive(true);
@@ -88,7 +137,21 @@ public class InteractableObjectScript : MonoBehaviour
             yield return new WaitForSeconds(InfoTime);
         }
         InfoScreen.SetActive(false);
-        monsterComing = true;
-        rawr.Play();
+
+        // Below Should be removed and placed somewhere in monster script
+        //monsterComing = true;
+        //rawr.Play();
+    }
+
+    private IEnumerator DisplayFlashback()
+    {
+        FlashbackScreen.SetActive(true);  // activates flashback screen 
+        var ImageDisplay = FlashbackScreen.GetComponentInChildren<Image>();  // gets the empty image area where flashback stills will be placed
+        foreach (var Still in FlashbackImages)  // iterates through all images and displays them in turn
+        {
+            ImageDisplay.sprite = Still;
+            yield return new WaitForSeconds(FlashbackTime);
+        }
+        FlashbackScreen.SetActive(false);  // turns screen off when done
     }
 }
