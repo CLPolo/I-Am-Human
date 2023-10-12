009c76d9d932f964f05c676397a328561181d5f2
unknown <mbmartin@ualberta.ca>
2023-10-11 23:29:51 -0600

    added a puzzle target that deletes an affected object (in this case a simulated door) and improved box collision handling using layers

---
+++ Unity/TechDemo/Assets/Scripts/PuzzleTargetScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTargetScript : MonoBehaviour
+{
+    public GameObject AffectedObject = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Box" ||  other.gameObject.tag == "Moveable")
+        {
+            if (AffectedObject != null)
+            {
+                Destroy(AffectedObject);
+            }
+        }
+    }
+
+
+}
