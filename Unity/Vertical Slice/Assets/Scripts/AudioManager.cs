79b8d57e7923774c32edfc4898eb944641908096
Corey <corey@pop-os.localdomain>
2023-10-28 02:20:55 -0600

    switching from Title to VC works in terms of ambient fades etc.

--- Unity/Vertical Slice/Assets/Scripts/AudioManager.cs
+++ Unity/Vertical Slice/Assets/Scripts/AudioManager.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public class Fade {
+    public static IEnumerator Start(AudioSource audioSource, float targetVolume = 1f, float duration = 3f)
+    {   //taken from https://johnleonardfrench.com/how-to-fade-audio-in-unity-i-tested-every-method-this-ones-the-best/ 
+        float currentTime = 0;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            Debug.Log(audioSource.volume + " " + audioSource.clip.name);
+            yield return null;
+        }
+        yield break;
+    }
+}
+
 public class AudioManager : MonoBehaviour
 {
     private Player p;
@@ -23,7 +39,7 @@ public class AudioManager : MonoBehaviour
     }
 
     private Dictionary<string, AudioSource> srcs;
-
+    private Fade Fade;
 
     // Start is called before the first frame update
     void Start()
@@ -47,12 +63,12 @@ public class AudioManager : MonoBehaviour
         if (scene == "Title Screen")
         {   
             if (srcs.TryGetValue("BGM", out AudioSource bgm) && !bgm.isPlaying)
-                {
-                    bgm.Play();
+            {
+                bgm.Play();
+                StartCoroutine(Fade.Start(bgm, 0.25f, 1f));
 
-                }
+            }
         }
-        
     }
 
     // Update is called once per frame
@@ -84,59 +100,57 @@ public class AudioManager : MonoBehaviour
         string pathAmb = "Sounds/SoundEffects/Environment/";
         string pathCutscene = "Sounds/SoundEffects/Misc/";
 
-        StopAll(false);
+        StopAll(true);
+        foreach(KeyValuePair<string, AudioSource> src in srcs){
 
-        switch (scene){
-            case "Vertical Slice":
-                foreach(KeyValuePair<string, AudioSource> src in srcs)
-                {
-                    string name = src.Key;
+            string name = src.Key;
+            AudioSource s = src.Value; 
 
-                    if (name == "BGM") src.Value.clip = null;
+            switch (scene){
+ 
+                case "Vertical Slice":
+                    
+                    if (name == "BGM") s.clip = null;
+                    if (name == "Cutscene")
+                    {   
+                        s.clip = Resources.Load<AudioClip>(pathCutscene + "car-crash-comp");
+                        s.PlayOneShot(s.clip, 0.8f);
+                    } 
+                    if (name == "AmbArea") {
+                        s.clip = Resources.Load<AudioClip>(pathAmb + "Forest/forest_ambience");
+                        RestartSource(s, true, 0.25f, 15f);
+                    }
+                    if (name == "AmbMisc"){
+                        s.clip = Resources.Load<AudioClip>(pathAmb + "creepyambience");     
+                        RestartSource(s, true, 0.08f, 15f);        
+                    }  
+                    break;
+
+                case "Title Screen":
+
+                    if (name == "BGM") s.clip = (AudioClip)Resources.Load(pathBGM + "title-theme");
                     if (name == "Cutscene")
                     {
-                        src.Value.clip = Resources.Load<AudioClip>(pathCutscene + "car-crash-comp");
-                        src.Value.Play();
+                        // s.clip = (AudioClip)Resources.Load(pathCutscene + "car-crash-comp");
+                        // s.Play();
                     } 
-                    if (name == "AmbArea") src.Value.clip = Resources.Load<AudioClip>(pathAmb + "Forest/forest_ambience");
-                    if (name == "AmbMisc") src.Value.clip = Resources.Load<AudioClip>(pathAmb + "creepyambience");                         
-                }  
-                break;
-            case "Title Screen":
-                foreach(KeyValuePair<string, AudioSource>  src in srcs)
-                {
-                    string name = src.Key;
-
-                    if (name == "BGM") src.Value.clip = (AudioClip)Resources.Load(pathBGM + "title-theme");
-                    if (name == "Cutscene")
-                    {
-                        src.Value.clip = (AudioClip)Resources.Load(pathCutscene + "car-crash-comp");
-                        src.Value.Play();
-                    } 
-                    if (name == "AmbArea") src.Value.clip = (AudioClip)Resources.Load(pathAmb + "Forest/forest_ambience");
-                    if (name == "AmbMisc" )src.Value.clip = (AudioClip)Resources.Load(pathAmb + "creepyambience");
-                }
-                break;
+                    if (name == "AmbArea") s.clip = (AudioClip)Resources.Load(pathAmb + "Forest/forest_ambience");
+                    if (name == "AmbMisc" )s.clip = (AudioClip)Resources.Load(pathAmb + "creepyambience");
+                    break;
+            }
         }
     }
+
     void StopAll(bool fade = true)
     {   
-        if (fade)
-        {
-            foreach(KeyValuePair<string, AudioSource>  src in srcs) 
-                FadeOut(src.Value);
-        } else {
-
-            foreach(KeyValuePair<string, AudioSource>  src in srcs) 
+        foreach(KeyValuePair<string, AudioSource>  src in srcs) 
+        {   if (src.Key != "Cutscene"){
+                if (fade) StartCoroutine(Fade.Start(src.Value, 0.01f, 1.5f));
                 src.Value.Stop();
+            }
         }
     }
 
-    void FadeOut(AudioSource s)
-    {
-        while (s.volume > 0.01f) s.volume *= 1/2f;
-        s.Stop();
-    }
 
     void CheckScenewide(string scene)
     {   
@@ -144,11 +158,11 @@ public class AudioManager : MonoBehaviour
             //Check for cutscene audio before playing everything else
             if (srcs.TryGetValue("Cutscene", out AudioSource cutscene) && !cutscene.isPlaying) {
                 //play background music
-                if(srcs.TryGetValue("BGM", out AudioSource bgm) && !bgm.isPlaying && bgm.clip != null) bgm.PlayOneShot(bgm.clip, 1f);
+                if(srcs.TryGetValue("BGM", out AudioSource bgm) && !bgm.isPlaying && bgm.clip != null) RestartSource(bgm);
                 //play area ambience
-                if (srcs.TryGetValue("AmbArea", out AudioSource ambA) && !ambA.isPlaying && ambA.clip != null) ambA.PlayOneShot(ambA.clip, 1f);
+                if (srcs.TryGetValue("AmbArea", out AudioSource ambA) && !ambA.isPlaying && ambA.clip != null) RestartSource(ambA);
                 //play general ambience
-                if(srcs.TryGetValue("AmbMisc", out AudioSource ambM) && !ambM.isPlaying && ambA.clip != null) ambM.PlayOneShot(ambM.clip, 1f);
+                if(srcs.TryGetValue("AmbMisc", out AudioSource ambM) && !ambM.isPlaying && ambA.clip != null) RestartSource(ambM);
             }
         }
     }   
@@ -167,16 +181,19 @@ public class AudioManager : MonoBehaviour
 
     void PlayerFootfall(PlayerState state)
     {   
-        
+        if (state == PlayerState.Running) 
         {
-            if (state == PlayerState.Running) 
-            {
-                pA.PlayOneShot(p.footstepsRun[UnityEngine.Random.Range(0, p.footstepsRun.Capacity)]);
+            pA.PlayOneShot(p.footstepsRun[UnityEngine.Random.Range(0, p.footstepsRun.Capacity)]);
 
-            } else if (state == PlayerState.Walking && p.GetAnimationIndex().isOneOf(2, 11)) {
+        } else if (state == PlayerState.Walking && p.GetAnimationIndex().isOneOf(2, 11)) {
 
-                pA.PlayOneShot(p.footstepsWalk[UnityEngine.Random.Range(0, p.footstepsWalk.Capacity)], 0.25f);
-            }
-        }
+            pA.PlayOneShot(p.footstepsWalk[UnityEngine.Random.Range(0, p.footstepsWalk.Capacity)], 0.25f);
+        }   
+    }
+    void RestartSource(AudioSource s, bool fade = false, float targetVolume = 0.5f, float duration = 0f){
+            s.volume = 0.01f;
+            if (s.isPlaying) s.Stop();
+            s.Play();
+            if (fade) StartCoroutine(Fade.Start(s, targetVolume, duration));
     }
 }
