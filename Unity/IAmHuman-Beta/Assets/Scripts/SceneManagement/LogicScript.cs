2b30395946a2858d39e97f2e72a0c3e528f64c08
Jonathan Fjeld <jfjeld@ualberta.ca>
2023-11-18 16:52:40 -0700

    Added Kitchen stuff, Fixed camera in almost all scenes

--- Unity/IAmHuman-Beta/Assets/Scripts/SceneManagement/LogicScript.cs
+++ Unity/IAmHuman-Beta/Assets/Scripts/SceneManagement/LogicScript.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 public class LogicScript : MonoBehaviour
@@ -36,6 +37,8 @@ public class LogicScript : MonoBehaviour
     public float defaultMashTimer = 1.5f;  // If you don't mash for 1 seconds you die
     private float mashTimer;
     public bool trapKills;
+    public bool inGore = false;
+    private float playerYStart = 1000f;
 
 
     // Start is called before the first frame update
@@ -59,6 +62,10 @@ public class LogicScript : MonoBehaviour
         {
             player = Player.Instance;
         }
+        if (playerYStart == 1000f)
+        {
+            playerYStart = player.transform.position.y;
+        }
         // PUT ALL LOGIC HERE
         if (player.GetState() == PlayerState.Trapped)
         {   
@@ -73,11 +80,19 @@ public class LogicScript : MonoBehaviour
 
     public void MashTrap()
     {
-        if(trappedText != null)
+        if (trappedText != null)
         {
             trappedText.SetActive(true);
         }
         mashTimer -= Time.deltaTime;
+        if (inGore)
+        {
+            TilemapCollider2D tilemapCollider = GameObject.Find("Tilemap").GetComponent<TilemapCollider2D>();
+            tilemapCollider.enabled = false;  // Disable the collider so the player can fall in the goop
+            player.transform.position = new Vector3(player.transform.position.x,
+                        player.transform.position.y - Time.deltaTime * (1.5f / 2.5f),
+                        player.transform.position.z);
+        }
         if (mashTimer <= 0 && trapKills)
         {
             // If the player does not mash fast enough they die :(
@@ -92,6 +107,11 @@ public class LogicScript : MonoBehaviour
         {
             // The player escapes!
             mashTimer = defaultMashTimer;
+            if (inGore)
+            {
+                TilemapCollider2D tilemapCollider = GameObject.Find("Tilemap").GetComponent<TilemapCollider2D>();
+                tilemapCollider.enabled = true;  // Enable the collider so the player no fall in goop
+            }
             player.SetState(PlayerState.Idle);
             PlayerPrefs.SetInt("escaped", 1);  // Miriam uses this for the kitchen door trapped interaction
             if (trappedText != null)
@@ -107,7 +127,16 @@ public class LogicScript : MonoBehaviour
                     //audioSource.clip = Resources.Load<AudioClip>("Sounds/SoundEffects/Entity/Interactable/mud-trap-struggle-" + UnityEngine.Random.Range(0,5).ToString());
                     audioSource.PlayOneShot(Resources.Load<AudioClip>("Sounds/SoundEffects/Entity/Interactable/mud-trap-struggle-" + UnityEngine.Random.Range(0,5).ToString()), 0.5f);
                 }
-                mashTimer += 0.3f;  // Add 0.2 seconds to the timer
+                if (inGore)
+                {
+                    mashTimer += 3;  // This allows player to insta escape if in gore
+                    player.transform.position = new Vector3(player.transform.position.x,
+                        playerYStart, player.transform.position.z);
+                }
+                else
+                {
+                    mashTimer += 0.3f;  // Add 0.3 seconds to the timer
+                }
             }
         }
     }
